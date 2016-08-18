cc14f0bd69e6eef9b33fc44baab3a6f850bfed6a
Sam Feder <sam.feder@amcnetworks.com>
2016-08-18 12:56:03 -0400

    finalize alpha

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const philResponse = [
   'Sorry guys, i can’t talk rn — on my way to the hamptons',
   'I’m up on the 9th floor, did you know they have almond milk here? Ridiculous',
   'Headin to the ‘nox to work on my fitness, will be back on slack later!',
-  'DK run anybody?'
+  'DK run anybody?',
+  'Can\'t tonight - running a 5K'
 ];
 
 if (!process.env.PORT) throw Error('PORT missing but required');
@@ -21,7 +22,15 @@ let slapp = Slapp({
 
 var app = slapp.attachToExpress(express());
 
-slapp.message('@phillip', msg => {
+app.get('/', function (req, res) {
+  res.send('Hello')
+});
+
+slapp.message('phillip', msg => {
+  msg.say(_.shuffle(philResponse)[0]);
+});
+
+slapp.message('<@U0PG1GRLH>', msg => {
   msg.say(_.shuffle(philResponse)[0]);
 });
 
